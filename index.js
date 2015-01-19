71ce086caf7cd15dfecfd25e6d254112a442635b
Raynos <raynos2@gmail.com>
2015-01-19 01:41:11 -0800

    working code

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 'use strict';
 
-module.exports = nullLogtron;
+var RingBuffer = require('ringbufferjs');
 
-function nullLogtron() {
+module.exports = NullLogtron;
 
+function NullLogtron(capacity) {
+    if (!(this instanceof NullLogtron)) {
+        return new NullLogtron();
+    }
+
+    this._buffer = new RingBuffer(capacity || 50);
+}
+
+var proto = NullLogtron.prototype;
+
+proto._log = function log(level, msg, meta, cb) {
+    this._buffer.enq(
+        new NullLogtronRecord(level, msg, meta)
+    );
+    if (typeof cb === 'function') {
+        cb();
+    }
+};
+
+proto.trace = function trace(msg, meta, cb) {
+    this._log('trace', msg, meta, cb);
+};
+
+proto.debug = function debug(msg, meta, cb) {
+    this._log('debug', msg, meta, cb);
+};
+
+proto.info = function info(msg, meta, cb) {
+    this._log('info', msg, meta, cb);
+};
+
+proto.warn = function warn(msg, meta, cb) {
+    this._log('warn', msg, meta, cb);
+};
+
+proto.error = function error(msg, meta, cb) {
+    this._log('error', msg, meta, cb);
+};
+
+proto.fatal = function fatal(msg, meta, cb) {
+    this._log('fatal', msg, meta, cb);
+};
+
+function NullLogtronRecord(level, msg, meta) {
+    this.level = level;
+    this.msg = msg;
+    this.meta = meta;
 }
--- test/index.js
+++ test/index.js
@@ -2,9 +2,82 @@
 
 var test = require('tape');
 
-var nullLogtron = require('../index.js');
+var NullLogtron = require('../index.js');
 
-test('nullLogtron is a function', function t(assert) {
-    assert.equal(typeof nullLogtron, 'function');
+test('NullLogtron is a function', function t(assert) {
+    assert.equal(typeof NullLogtron, 'function');
     assert.end();
+});
+
+test('can allocate logger', function t(assert) {
+    var logger = NullLogtron();
+
+    assert.ok(logger.trace);
+    assert.ok(logger.debug);
+    assert.ok(logger.info);
+    assert.ok(logger.warn);
+    assert.ok(logger.error);
+    assert.ok(logger.fatal);
+
+    assert.end();
+});
+
+test('can call methods', function t(assert) {
+    /*eslint max-statements: 0 */
+    var logger = new NullLogtron();
+    var msg = 'hi';
+    var meta = {};
+
+    logger.trace(msg, meta);
+    logger.debug(msg, meta);
+    logger.info(msg, meta);
+    logger.warn(msg, meta);
+    logger.error(msg, meta);
+    logger.fatal(msg, meta);
+
+    var buffer = logger._buffer;
+    var elements = buffer._elements;
+
+    assert.equal(elements.length, 50);
+    var line1 = elements[0];
+    assert.equal(line1.msg, msg);
+    assert.equal(line1.meta, meta);
+    assert.equal(line1.level, 'trace');
+
+    var line2 = elements[1];
+    assert.equal(line2.msg, msg);
+    assert.equal(line2.meta, meta);
+    assert.equal(line2.level, 'debug');
+
+    var line3 = elements[2];
+    assert.equal(line3.msg, msg);
+    assert.equal(line3.meta, meta);
+    assert.equal(line3.level, 'info');
+
+    var line4 = elements[3];
+    assert.equal(line4.msg, msg);
+    assert.equal(line4.meta, meta);
+    assert.equal(line4.level, 'warn');
+
+    var line5 = elements[4];
+    assert.equal(line5.msg, msg);
+    assert.equal(line5.meta, meta);
+    assert.equal(line5.level, 'error');
+
+    var line6 = elements[5];
+    assert.equal(line6.msg, msg);
+    assert.equal(line6.meta, meta);
+    assert.equal(line6.level, 'fatal');
+
+    assert.end();
+});
+
+test('can call callback', function t(assert) {
+    var logger = NullLogtron();
+
+    logger.info('hi', {}, function onError(err) {
+        assert.ifError(err);
+
+        assert.end();
+    });
 });
